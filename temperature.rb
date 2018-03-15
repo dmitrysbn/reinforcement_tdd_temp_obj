0587cc306b98c09f79d534824c48f2ba4bee4336
dmitrysbn <dmitry.sbn@gmail.com>
2018-03-15 11:37:54 -0400

    Refactor solution

--- temperature.rb
+++ temperature.rb
@@ -1,31 +1,16 @@
 class Temperature
 
   def initialize(temperature = {})
-    @temperature = temperature
-  end
-
-  def fahrenheit
-    @temperature[:f]
-  end
-
-  def celsius
-    @temperature[:c]
+    @fahrenheit = temperature[:f]
+    @celsius = temperature[:c]
   end
 
   def to_fahrenheit
-    if fahrenheit
-      fahrenheit
-    elsif celsius
-      celsius * 9 / 5 + 32
-    end
+    @fahrenheit || @celsius * 9 / 5 + 32
   end
 
   def to_celsius
-    if celsius
-      celsius
-    elsif fahrenheit
-      (fahrenheit - 32) * 5 / 9
-    end
+    @celsius || (@fahrenheit - 32) * 5 / 9
   end
 
 end
